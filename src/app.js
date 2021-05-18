1d75ef79618afe49513eac127d03936fb790da6c
mohammed-khamees <mohammed.khamis1996@yahoo.com>
2021-05-19 00:48:33 +0300

    context

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ToDo from './components/todo/todo.js';
 import NavBar from './components/todo/navbar.js';
+// import Main from './components/todo/main.js';
+
+// import SortProvider from './components/context/sort';
+import PaginationProvider from './components/context/pagination';
 
 const App = () => (
 	<>
 		<NavBar />
-		<ToDo />
+		{/* <SortProvider> */}
+		<PaginationProvider>
+			<ToDo />
+		</PaginationProvider>
+		{/* </SortProvider> */}
 	</>
 );
 
--- src/components/todo/list.js
+++ src/components/todo/list.js
@@ -1,35 +1,42 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Toast, Badge, ListGroup } from 'react-bootstrap';
+import { PaginationContext } from './../context/pagination';
 
 const TodoList = (props) => {
+	const paginationContext = useContext(PaginationContext);
+
 	return (
 		<ListGroup>
-			{props.list.map((item) => (
-				<Toast
-					className={`complete-${item.complete.toString()}`}
-					key={item._id}
-					onClose={() => {
-						props.handleDelete(item._id, 'delete');
-					}}
-				>
-					<Toast.Header>
-						<Badge pill variant={item.complete ? 'success' : 'danger'}>
-							{item.complete ? 'Pending' : 'Complete'}
-						</Badge>
-						<strong className="mr-auto" style={{ marginLeft: '20px' }}>
-							{item.assignee}
-						</strong>
-					</Toast.Header>
-					<Toast.Body
-						onClick={() => {
-							props.handleComplete(item._id, 'put');
+			{paginationContext.items
+				.slice(paginationContext.offset, paginationContext.offset + 3)
+				.map((item) => (
+					<Toast
+						className={`complete-${item.complete.toString()}`}
+						key={item._id}
+						onClose={() => {
+							props.handleDelete(item._id, 'delete');
 						}}
 					>
-						{item.text}
-						<div class="difficulty">Difficulty: {item.difficulty}</div>
-					</Toast.Body>
-				</Toast>
-			))}
+						<Toast.Header>
+							<Badge
+								pill
+								variant={!item.complete ? 'success' : 'danger'}
+								onClick={() => {
+									props.handleComplete(item._id, 'put');
+								}}
+							>
+								{!item.complete ? 'Pending' : 'Complete'}
+							</Badge>
+							<strong className="mr-auto" style={{ marginLeft: '20px' }}>
+								{item.assignee}
+							</strong>
+						</Toast.Header>
+						<Toast.Body>
+							{item.text}
+							<div className="difficulty">Difficulty: {item.difficulty}</div>
+						</Toast.Body>
+					</Toast>
+				))}
 		</ListGroup>
 	);
 };
